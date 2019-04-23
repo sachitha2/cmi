8b6c49c89b250ad0c5244d216690673178a5f959
sachitha2 <shpsachitha@gmail.com>
2019-04-23 23:40:18 +0530

    Working on credits page

---
+++ en/admin/html/subPages/viewCreditsWeek.php
@@ -0,0 +1,12 @@
+<?php
+require_once("../db.php");
+require_once("../../methods/DB.class.php");
+require_once("../../methods/Main.class.php");
+$DB = new DB;
+$DB->conn = $conn;
+$main = new Main;
+?>
+<?php $main->b("credits.php") ?>
+	<h2>Week credits</h2>
+	
+	
