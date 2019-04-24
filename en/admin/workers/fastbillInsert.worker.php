047ff9f7e9c72043334c41597b2418e546b69cef
sachitha2 <shpsachitha@gmail.com>
2019-04-24 16:22:29 +0530

    fast bill

--- en/admin/workers/fastbillInsert.worker.php
+++ en/admin/workers/fastbillInsert.worker.php
@@ -5,7 +5,6 @@ print_r($postData);
 $itemId = $postData['itemId'];
 $qty = $postData['qty'];
 $billNumber = $postData['billNumber'];
-
 $conn->query("INSERT INTO purchaseditems (id, dealid, itemid, amount, uprice, stockid, type) VALUES (NULL, '$billNumber', '$itemId', '$qty', '50', '10', '1');");
 $conn->close();
 ?>
