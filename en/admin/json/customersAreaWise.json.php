e588f1ed5b36b04dcaefe146d0529d6453466e00
Sandalisenarathne <sandalisenarathna@gmail.com>
2019-04-27 13:25:43 +0530

    customersAreaWise JSON

---
+++ en/admin/json/customersAreaWise.json.php
@@ -0,0 +1,69 @@
+<?php
+
+date_default_timezone_set("Asia/Kolkata");
+
+$dt_min = new DateTime("last saturday");
+$dt_min->modify('+1 day');
+$dt_max = clone($dt_min);
+$dt_max->modify('+6 days');
+
+$week = $dt_min->format('Y-m-d').' to '.$dt_max->format('Y-m-d').')';
+
+require_once("../html/db.php");
+require_once("../methods/DB.class.php");
+
+// 1 for Today/Yesterday
+// 2 for This week/Last week
+// 3 for This month/Last month
+// 4 for This year/Last year
+$Query = htmlspecialchars($_GET["query"]);
+
+$DB = new DB;
+$DB->conn = $conn;
+$arr = $DB->select("area","");
+
+$x = 0;
+
+if($Query == 1){
+	$logicThis = "&& regdate = CURDATE()";
+	$logicLast = "&& regdate = CURDATE()-1";
+}elseif($Query == 2){
+	$logicThis = "&& WEEK(regdate) = WEEK(CURDATE()) && MONTH(regdate) = MONTH(CURDATE()) && YEAR(regdate) = YEAR(CURDATE())";
+	$logicLast = "&& WEEK(regdate) = WEEK(CURDATE())-1 && MONTH(regdate) = MONTH(CURDATE())-1 && YEAR(regdate) = YEAR(CURDATE())-1";
+}elseif($Query == 3){
+	$logicThis = "&& MONTH(regdate) = MONTH(CURDATE()) && YEAR(regdate) = YEAR(CURDATE())";
+	$logicLast = "&& MONTH(regdate) = MONTH(CURDATE())-1 && YEAR(regdate) = YEAR(CURDATE())-1";
+}elseif($Query == 4){
+	$logicThis = "&& YEAR(regdate) = YEAR(CURDATE())";
+	$logicLast = "&& YEAR(regdate) = YEAR(CURDATE())-1";
+}
+
+foreach ($arr as $data) {
+	
+	$numOfActivesT = $DB->nRow("customer","WHERE (areaid = ".$data['id']." && status = 1 ".$logicThis." )");
+	$numOfInactivesT = $DB->nRow("customer","WHERE (areaid = ".$data['id']." && status = 0 ".$logicThis." )");
+	
+	$numOfActivesL = $DB->nRow("customer","WHERE (areaid = ".$data['id']." && status = 1 ".$logicLast." )");
+	$numOfInactivesL = $DB->nRow("customer","WHERE (areaid = ".$data['id']." && status = 0 ".$logicLast." )");
+	
+	$customersArray['This']['areaId'][$x] = $data['id'];
+	$customersArray['This']['area'][$x] = $data['name'];
+	$customersArray['This']['activeCustomers'][$x] = $numOfActivesT;
+	$customersArray['This']['inactiveCustomers'][$x] = $numOfInactivesT;
+	
+	$customersArray['Last']['areaId'][$x] = $data['id'];
+	$customersArray['Last']['area'][$x] = $data['name'];
+	$customersArray['Last']['activeCustomers'][$x] = $numOfActivesL;
+	$customersArray['Last']['inactiveCustomers'][$x] = $numOfInactivesL;
+	
+	$customersArray['Comparison']['activeIncrement'][$x] = $customersArray['This']['activeCustomers'][$x] - $customersArray['Last']['activeCustomers'][$x];
+	
+	$customersArray['Comparison']['inactiveIncrement'][$x] = $customersArray['This']['inactiveCustomers'][$x] - $customersArray['Last']['inactiveCustomers'][$x];
+	
+	$x++;
+	
+}
+
+$json = json_encode($customersArray);
+echo($json);
+?>
