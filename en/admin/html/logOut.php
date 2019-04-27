5b8491ace6bcf04a2c61b4f8ad42c3a236fe2814
sachitha2 <shpsachitha@gmail.com>
2019-04-27 15:55:52 +0530

    Adding Log out button to profile.php page and adding logut redirect url to logout.php

--- en/admin/html/logOut.php
+++ en/admin/html/logOut.php
@@ -1,7 +1,10 @@
 <?php
 			session_start();
+
+			$logOutUrl = "../../../L";
 			$_SESSION['login']['pass'] = 1000;
 			$_SESSION['login']['status'] = 0;
 			$_SESSION['login']['user'] = "";
 			print_r($_SESSION['login']);
+			header("location:$logOutUrl");
 ?>
