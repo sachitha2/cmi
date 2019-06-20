45e2b8264cc62f64f4b0da6b4b91233c6a2f70b6
sachitha2 <shpsachitha@gmail.com>
2019-06-20 16:30:03 +0530

    Adding find customer by cid to sell credit

--- en/admin/html/subPages/sellCustomer.php
+++ en/admin/html/subPages/sellCustomer.php
@@ -8,14 +8,38 @@ $main = new Main;
 $main->b("sell.php");
 $customer = $DB->select("customer","");
 ?>
-			<h1>Select Customer</h1>
+			<h1>Select Customer From NIC</h1>
 			<input list="colors" name="color" id="idCard" class="form-control" style="width: 200px" onKeyPress="enterCheckCustomerForMakeBill(event,this.value)">
 			<datalist id="colors">
 				
     			<?php
 					foreach($customer as $data){
+						if($data['nic'] != "0000000000"){
+							?>
+							<option value="<?php echo($data['nic']) ?>"><?php echo($data['name']) ?><?php // $DB->getcustomerNameByStockId($data['id']) ?></option>
+						
+							<?php
+						}
+						
+					}
+	
+				?>
+			</datalist>
+			<div id="msg"></div>
+			<br>
+			<input type="button" value="Next"  class="btn btn-primary btn-lg" onClick="CheckCustomerForMakeBill(idCard.value);">
+			
+			
+			
+			<!---///TODO--->
+			<h1>Select Customer From CID</h1>
+			<input list="cids" name="color" id="cid" class="form-control" style="width: 200px" onKeyPress="enterCheckCustomerForMakeBill(event,this.value)">
+			<datalist id="cids">
+				
+    			<?php
+					foreach($customer as $data){
 						?>
-						<option value="<?php echo($data['nic']) ?>"><?php echo($data['name']) ?><?php // $DB->getcustomerNameByStockId($data['id']) ?></option>
+						<option value="<?php echo($data['id']) ?>"><?php echo($data['name']) ?><?php // $DB->getcustomerNameByStockId($data['id']) ?></option>
 						
 						<?php
 					}
