45d0f8f5c981ac8962423ca30e3c3bd2ec360463
sachitha2 <shpsachitha@gmail.com>
2019-06-20 16:48:28 +0530

    Created order page

--- en/admin/methods/Main.class.php
+++ en/admin/methods/Main.class.php
@@ -245,6 +245,16 @@ class Main{
                     <span class="nav-text">Sales</span>
                   </a>
                 </li>
+                 <li>
+                  <a onClick="window.location.assign('order.php')">
+                    <span class="nav-icon">
+                      <i class="material-icons">&#xe870;
+                        <span ui-include="'../assets/images/i_7.svg'"></span>
+                      </i>
+                    </span>
+                    <span class="nav-text">Orders</span>
+                  </a>
+                </li>
                 
             
               </ul>
