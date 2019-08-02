99d1f9acfefbacd3dffe8075857149226cd6e675
Sandali Senarathna <sandalisenarathna@gmail.com>
2019-08-02 15:07:31 +0530

    View salary for admins and agents

--- en/admin/html/subPages/viewSalary.php
+++ en/admin/html/subPages/viewSalary.php
@@ -15,49 +15,60 @@ $DB->conn = $conn;?>
 <?php
 	if($DB->nRow("salary","") != 0){	?>
 	
-			<table class="table table-hover table-bordered table-striped table-dark">
-  <thead class="thead-dark">
-    <tr>
-      <th id="id" scope="col" width="10">ID</th>
-      <th id="empId" scope="col">Employee ID</th>
-      <th id="empName" scope="col">Name</th>
-      <th id="amount" scope="col">Amount</th>
-      <th id="costId" scope="col">Cost ID</th>
-      <th id="date" scope="col">Date</th>
-      <th id="time" scope="col">Time</th>
-    </tr>
-  </thead>
-  <tbody>
-    
-    <?php
-			$arr = $DB->select("salary","");
-//	  		print_r($arr);
-			foreach($arr as $data){
-				?>
-				<tr>
-					<td scope="row"><?php echo($data['id']) ?></td>
-					<td><?php echo($data['employeeId'] )?></td>
-					<?php
-					$empId = $data['employeeId'];
-					$arrTemp = $DB->select("user","WHERE id = $empId");
-					foreach($arrTemp as $dataTemp){
-					?>
-						<td><?php echo($dataTemp['username'])?></td>
-					<?php } ?>
-					<td><?php echo($data['amount']) ?></td>
-					<td><?php echo($data['costId']) ?></td>
-					<td><?php echo($data['date']) ?></td>
-					<td><?php echo($data['time']) ?></td>
+	<table class="table table-hover table-bordered table-striped table-dark">
+		<thead class="thead-dark">
+			<tr>
+			<th id="id" scope="col" width="10">ID</th>
+			<th id="empId" scope="col">Employee ID</th>
+			<th id="empName" scope="col">Name</th>
+			<th id="amount" scope="col">Amount</th>
+			<th id="costId" scope="col">Cost ID</th>
+			<th id="date" scope="col">Date</th>
+			<th id="time" scope="col">Time</th>
+			</tr>
+		</thead>
+		<tbody>
+			
+			<?php
+			
+					if($_SESSION["login"]['type'] == 1){
+						$arr = $DB->select("salary","");
+					}else if($_SESSION["login"]['type'] == 2){
 
-					<!--<td><button type="button" class="btn btn-md btn-primary">Edit</button></td>
-					<td><button onClick="delCustomer(<?php //echo($data['id']) ?>)" type="button" class="btn btn-md btn-danger ">X</button></td>-->
-					
-				</tr>
-				<?php
-			}
-		?>
-  </tbody>
-</table>
+						$user = $_SESSION["login"]['user'];
+						$arrTemp1 = $DB->select("user","WHERE username = '$user'");
+						foreach($arrTemp1 as $dataTemp1){
+							$id = $dataTemp1['id'];	
+						} 
+						$arr = $DB->select("salary","WHERE employeeId = $id");
+					}
+	
+					foreach($arr as $data){
+			?>
+						<tr>
+							<td scope="row"><?php echo($data['id']) ?></td>
+							<td><?php echo($data['employeeId'] )?></td>
+							<?php
+							$empId = $data['employeeId'];
+							$arrTemp2 = $DB->select("user","WHERE id = $empId");
+							foreach($arrTemp2 as $dataTemp2){
+							?>
+								<td><?php echo($dataTemp2['username'])?></td>
+							<?php } ?>
+							<td><?php echo($data['amount']) ?></td>
+							<td><?php echo($data['costId']) ?></td>
+							<td><?php echo($data['date']) ?></td>
+							<td><?php echo($data['time']) ?></td>
+
+							<!--<td><button type="button" class="btn btn-md btn-primary">Edit</button></td>
+							<td><button onClick="delCustomer(<?php //echo($data['id']) ?>)" type="button" class="btn btn-md btn-danger ">X</button></td>-->
+							
+						</tr>
+					<?php
+					}
+					?>
+		</tbody>
+	</table>
 	
 	<?php
 		
