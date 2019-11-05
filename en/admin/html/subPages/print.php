d690e1683b4cfba0f9b514fd19807cb69981bcfd
Sandali Senarathna <sandalisenarathna@gmail.com>
2019-11-05 21:16:29 +0530

    navigation of reports and bdaybook, print.php

--- en/admin/html/subPages/print.php
+++ en/admin/html/subPages/print.php
@@ -5,16 +5,11 @@
 <html>
 <head>
 <!--  PDF-->
-  <link rel="stylesheet" href="https://printjs-4de6.kxcdn.com/print.min.css" type="text/css" />
-  
-  <script src="https://printjs-4de6.kxcdn.com/print.min.js"></script>
+<link rel="stylesheet" href="https://printjs-4de6.kxcdn.com/print.min.css" type="text/css" />
+<script src="https://printjs-4de6.kxcdn.com/print.min.js"></script>
 <meta charset="utf-8">
-<title>print</title>
-<style>
-	.title{
-		
-	}	
-</style>
+<title>Print Invoice</title>
+<style></style>
 <script>
 	var someJSONdata = [
     {
@@ -35,40 +30,51 @@
        Qty: '333-333-3333',
 	   Amount: '111-111-1111'
     }
- ];
+ 	];
 	var head = '<h3 style="font-size: 50px;margin:20px">TRANS LANKA</h3><P>Address - my bussiness address<br>Tel - 0715591137/0771466460<br>Email - youremail@mail.com</p>';
 </script>
 </head>
 
 <body onLoad="">
-	<h1 align="left" style="font-size: 50px;margin: 20px">INVOICE</h1>
+	<h1 align="center" style="font-size: 50px;margin: 20px">INVOICE</h1>
 	
 	
 	
-	<table border="0">
+	<table align="center" border="0" width="80%">
 		<tr>
-			<th><h1 align="left" style="font-size: 15px;padding-right: 20px">INVOICE NUMBER <br>1570115091252 </h1></th>
-			<td><h1 align="left" style="font-size: 15px;">Date of issue <br>2019-10-10 </h1></td>
+			<th valign="top" align="left" width="50%">INVOICE NUMBER</th>
+			<th valign="top" align="left" width="50%">Date of issue</th>
+		</tr>
+		<tr>
+			<td valign="top" align="left">1570115091252</td>
+			<td valign="top" align="left">2019-10-10</td>
 		</tr>
 	</table>
-	<table border="0" width="100%">
+	<br><br>
+	<table align="center" border="0" width="80%">
+	 	<tr>
+		 	<th valign="top" align="left" width="50%">Billed to : </th>
+			<th valign="top" align="left" width="50%">Your Company Name</th>
+		</tr>
 		<tr>
-			<td valign="top">
-				<br>
-				<b>Billed to</b>
-				<br>
-				Client name
-				<br>
-				Address
-			</td>
-			<td valign="top">
-				<b>Your Company name</b><br>
-				Address<br>
-				Tel1<br>
-				Tel2<br>
-				Mail<br>
-				web<br>
-			</td>
+			<td valign="top" align="left">Client name</td>
+			<td valign="top" align="left">Address</td>
+		</tr>
+		<tr>
+			<td valign="top" align="left">Address</td>
+			<td valign="top" align="left">Tel1</td>
+		</tr>
+		<tr>
+			<td valign="top" align="left">&nbsp</td>
+			<td valign="top" align="left">Tel2</td>
+		</tr>
+		<tr>
+			<td valign="top" align="left">&nbsp</td>
+			<td valign="top" align="left">Mail</td>
+		</tr>
+		<tr>
+			<td valign="top" align="left">&nbsp</td>
+			<td valign="top" align="left">Web</td>
 		</tr>
 	</table>
 	
